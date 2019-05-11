44fd22268642cf8f0ab7d5dbe932b56b4e59db90
AaronYen <clove2471@gmail.com>
2019-05-11 12:36:50 +0800

    add count

--- r.py
+++ r.py
@@ -3,14 +3,17 @@ import random
 r = random.randint(1,100)
 min_num = 1;
 max_num = 100;
+count = 0;
+
 while True:
-	num = int(input('請猜數字 1-100 :'))
+	count = count +1
+	num = int(input('請猜 %d - %d 數字:' %(min_num, max_num)))
 	if num == r:
-		print('猜對了, 答案是', r)
+		print('猜對了, 答案是 %d ,總共猜了 %d 次' %(r, count))
 		break
 	elif num < r:
 		min_num = num+1
 	elif num > r:
 		max_num = num-1
 
-	print('請猜 %d - %d 數字' %(min_num, max_num))
+	
